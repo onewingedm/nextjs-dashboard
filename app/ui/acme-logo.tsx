0abe57283fc37376f7a19762e1cab688eff4aa7a
onewingedm <50483362+onewingedm@users.noreply.github.com>
2025-08-21 14:27:03 +0300

    Update acme-logo.tsx

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,1 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { iusitana } from './fonts';
 
-export default function AcmeLogo() {
-  return (
-    <div
-      className={`${iusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
-    </div>
-  );
-}
